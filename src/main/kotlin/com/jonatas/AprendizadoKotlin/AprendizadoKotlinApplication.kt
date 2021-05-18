ea3200a7d787ef20921652df9e2cd34a20849d3a
jojoccp <jonatas.ccp@gmail.com>
2021-05-18 15:10:52 -0300

    adicionado projeto

---
+++ src/main/kotlin/com/jonatas/AprendizadoKotlin/AprendizadoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.jonatas.AprendizadoKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AprendizadoKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<AprendizadoKotlinApplication>(*args)
+}
